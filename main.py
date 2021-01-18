148b1d90285d0ce1cf8cabf4ed4a9c07b81a1c9d
nmparfait <mbamu.developer@gmail.com>
2021-01-18 17:28:25 +0000

    starting a new way of generating a password

--- main.py
+++ main.py
@@ -11,15 +11,28 @@ print("____Welcome to the PyPassword Generator!_____\n")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
+#easy way 
 
-password = ""
+#password = ""
+#for char in range(1, nr_letters + 1):
+#  password += random.choice(letters)
+
+#for char in range(1, nr_numbers + 1):
+#  password += random.choice(numbers)
+
+#for char in range(1, nr_symbols + 1):
+#  password += random.choice(symbols)
+#print(password)
+
+#hard way 
+password_list = []
+
 for char in range(1, nr_letters + 1):
-  password += random.choice(letters)
+  password_list.append(random.choice(letters))
 
 for char in range(1, nr_numbers + 1):
-  password += random.choice(numbers)
+  password_list.append(random.choice(numbers))
 
 for char in range(1, nr_symbols + 1):
-  password += random.choice(symbols)
-
-print(password)
+  password_list.append(random.choice(symbols))
+print(password_list)
